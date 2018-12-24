df82612cc86e4bbb0445d2b87509a05bb14880f7
Tanvi Potdar <tanvipotdar@tanvis-imac.mynet>
2018-12-24 07:45:39 +0000

    new subquestions

--- financial_data_analysis.py
+++ financial_data_analysis.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
+from statsmodels.tsa.ar_model import AR
+from statsmodels.tsa.arima_model import ARMA
 
 #helper functions
  def f(x):            
@@ -8,8 +11,8 @@ import matplotlib.pyplot as plt
 		x = x.replace(year=x.year-100)
 	return x
 
-
-gnp_data = pd.read_csv('/Users/tanvi/Projects/modelling/GNP.csv', index_col='DATE')
+# 1.1
+gnp_data = pd.read_csv('/Users/tanvipotdar/Projects/Advanced-Numerical-Methods/GNP.csv', index_col='DATE')
 log_returns = np.log(gnp_data.GNP/gnp_data.shift(1).GNP)*100
 log_returns.fillna(1.0, inplace=True)
 dates_index = pd.to_datetime(log_returns.index)
@@ -19,11 +22,20 @@ log_returns.index = dates
 training_data = log_returns[:232]
 testing_data = log_returns[232:]
 
+
 # plotting training data log returns
 training_returns_plot = training_data.plot()
 training_returns_plot.set(xlabel="Time(Quarter)", ylabel="GNP quarterly growth rate", title="Log returns over time")
 
-#1.2 Exponential Smoothing (b)
+# plotting training data acf
+acf_plot = plot_acf(training_data)
+acf_plot.set(xlabel="Lags", ylabel="Autocorrelation", title="Autocorrelation function for log returns")
+
+# plotting training data pacf
+pacf_plot = plot_pacf(training_data)
+pacf_plot.set(xlabel="Lags", ylabel="Partial Autocorrelation", title="Partial Autocorrelation function for log returns")
+
+# 1.2 Exponential Smoothing 
 ses = training_data.to_frame('y')
 alpha_values = np.arange(0.01, 1.01, 0.01)
 
@@ -44,6 +56,40 @@ for alpha in alpha_values:
 
  errors['sse'] = error_vals
  errors.set_index('alpha', inplace=True)
- optimal_alpha = errors.idxmin()
- sse_plot = errors.plot()
- sse_plot.set(xlabel="Alpha", ylabel="Sum of squared errors", title="SSE for alpha values")
+
+# plot of alpha values versus in sample SSE
+sse_plot = errors.plot()
+sse_plot.set(xlabel="Alpha", ylabel="Sum of squared errors", title="SSE for alpha values")
+
+# optimal value of alpha that minimises in sample SSE
+optimal_alpha = errors.idxmin()
+
+# plot of in-sample model response vs optimal alpha 
+optimal_df = ses[['y', 's_0.52']]
+optimal_df = optimal_df.rename(columns={'y':'Actual GNP Returns', 's_0.52':'GNP Returns using optimal alpha'})
+optimal_plot = optimal_df.plot()
+optimal_plot.set(xlabel="Time(Quarter)", ylabel="SES Model GNP Returns", title="Optimal SES vs Actual GNP Returns")
+
+# 1.3 Autoregressive models
+
+# ar_model = AR(training_data)
+# min_aic = 20000000
+# min_order=0
+# for p in range(1,21):
+# 	model_fit = ar_model.fit(p)
+# 	model_aic = model_fit.aic
+# 	if model_aic < min_aic:
+# 		min_aic = model_aic
+# 		min_order = p
+
+
+min_aic = 20000000
+min_order=0
+for p in range(1,21):
+	model = ARMA(training_data, order=(p,0))
+	model_fit = model.fit()
+	model_aic = model_fit.aic
+	if model_aic < min_aic:
+		min_aic = model_aic
+		min_order = p
+
