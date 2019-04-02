4a803c5a8f4628d1b33a661eef490eb42bfd0638
Tanvi Potdar <tanvipotdar@tanvis-imac.mynet>
2019-04-03 00:18:19 +0100

    added plots

--- module 7/optimal_execution.py
+++ module 7/optimal_execution.py
@@ -1,5 +1,8 @@
 
 import numpy as np
+from matplotlib import pyplot as plt
+import pandas as pd
+
 
 # define all parameters
 T = 1  # expiry (hours)
@@ -14,11 +17,11 @@ k_mu = 1e-3  # long running mean of k
 k_sigma = 1.5e-3  # vol of k
 
 b_eta = 10  # mean reversion rate of b
-b_mu = 2e-3  # long running mean of b
+b_mu = 1e-3  # long running mean of b
 b_sigma = 3e-3  # vol of b
 
 
-def run_almgren_chriss_with_constant_pi():
+def run_almgren_chriss_with_constant_pi(alpha=alpha, phi=phi, T=T, k_mu=k_mu, b_mu=b_mu, s_sigma=s_sigma):
     # time in minutes
     t = T * 60
 
@@ -35,13 +38,14 @@ def run_almgren_chriss_with_constant_pi():
     zeta = (alpha - 0.5 * b_mu + np.sqrt(phi * k_mu)) / (alpha - 0.5 * b_mu - np.sqrt(phi * k_mu))
 
     for i in range(t + 1):
-        q[i] = N if i == 0 else q[i - 1] - v[i - 1]
-        s[i] = s0 if i == 0 else s[i - 1] - (b_mu * v[i - 1] / 60 + s_sigma * w[i] * np.sqrt(i / 60.))
+        q[i] = N if i == 0 else q[i - 1] - min(v[i - 1], q[i-1])
+        s[i] = s0 if i == 0 else s[i - 1] - (b_mu * v[i - 1] / 60. + s_sigma * w[i])
 
         tT = (t - i) / float(t)
         v_num = zeta * np.exp(gamma * tT) + np.exp(-gamma * tT)
-        v_denom = zeta * np.exp(gamma * T) - np.exp(-gamma * T)
+        v_denom = zeta * np.exp(gamma * tT) - np.exp(-gamma * tT)
         v[i] = gamma * (v_num / v_denom) * q[i]
+        # divide by 60 as dt=1/60
         v[i] = max(v[i], 0) / 60.
         e[i] = s[i] - k_mu * v[i]
         if i == t:
@@ -51,8 +55,7 @@ def run_almgren_chriss_with_constant_pi():
             x[i] = e[i] * min(v[i], q[i])
 
     x[t] = q[t] * (s[t] - alpha * q[t])
-    terminal_x = np.sum(x)
-    return terminal_x
+    return x.cumsum(), q
 
 
 def calculate_stochastic_path(mu, eta, sigma):
@@ -68,7 +71,8 @@ def calculate_stochastic_path(mu, eta, sigma):
     return pi_path
 
 
-def run_almgren_chriss_with_stochastic_pi():
+def run_almgren_chriss_with_stochastic_pi(alpha=alpha, phi=phi, T=T, k_mu=k_mu, k_eta=k_eta, k_sigma=k_sigma, 
+	b_mu=b_mu, b_eta=b_eta, b_sigma=b_sigma, s_sigma=s_sigma):
     # time in minutes
     t = T * 60
 
@@ -97,7 +101,7 @@ def run_almgren_chriss_with_stochastic_pi():
 
         tT = (t - i) / float(t)
         v_num = zeta[i] * np.exp(gamma[i] * tT) + np.exp(-gamma[i] * tT)
-        v_denom = zeta[i] * np.exp(gamma[i] * T) - np.exp(-gamma[i] * T)
+        v_denom = zeta[i] * np.exp(gamma[i] * tT) - np.exp(-gamma[i] * tT)
         v[i] = gamma[i] * (v_num / v_denom) * q[i]
         v[i] = max(v[i], 0) / 60.
         e[i] = s[i] - k[i] * v[i]
@@ -108,8 +112,7 @@ def run_almgren_chriss_with_stochastic_pi():
             x[i] = e[i] * min(v[i], q[i])
 
     x[t] = q[t] * (s[t] - alpha * q[t])
-    terminal_x = np.sum(x)
-    return terminal_x
+    return x.cumsum(), q
 
 
 def calculate_performance(mc_paths):
@@ -118,9 +121,66 @@ def calculate_performance(mc_paths):
     performance = np.zeros(mc_paths)
 
     for x in range(mc_paths):
-        cash_from_constant_strategy[x] = run_almgren_chriss_with_constant_pi()
-        cash_from_stochastic_strategy[x] = run_almgren_chriss_with_stochastic_pi()
+        cash_from_constant_strategy[x] = run_almgren_chriss_with_constant_pi()[0][-1]
+        cash_from_stochastic_strategy[x] = run_almgren_chriss_with_stochastic_pi()[0][-1]
         performance[x] = (cash_from_stochastic_strategy[x] - cash_from_constant_strategy[x]) / cash_from_constant_strategy[x]
         performance[x] *= 10000
 
+
     return performance
+
+
+def plot_all():
+	# histogram of performance
+	histogram, ax = plt.subplots(figsize=(10,5))
+	ax.set(ylabel='Frequency', xlabel='Performance (bps)', title='Performance of stochastic vs constant AC')
+	histogram = plt.hist(a, density=False)
+	plt.show()
+
+	# plot inventory process with different alphas
+	a0001,b0001 = run_almgren_chriss_with_constant_pi(alpha=0.0001)
+	a001,b001 = run_almgren_chriss_with_constant_pi(alpha=0.001)
+	a01,b01=run_almgren_chriss_with_constant_pi(alpha=0.01)
+	a1,b1=run_almgren_chriss_with_constant_pi(alpha=0.1)
+
+	f, ((ax1, ax2)) = plt.subplots(1, 2, sharex='col')
+	ax1.set(ylabel='Inventory', xlabel='time (minutes)')
+	ax1.plot(b0001, label='alpha=0.0001')
+	ax1.plot(b001, label='alpha=0.001')
+	ax1.plot(b01, label='alpha=0.01')
+	ax1.plot(b1, label='alpha=0.1')
+	ax1.legend(loc="upper right")
+
+	ax2.set(ylabel='Income earned', xlabel='time (minutes)')
+	ax2.plot(a0001[:-1], label='alpha=0.0001')
+	ax2.plot(a001[:-1], label='alpha=0.001')
+	ax2.plot(a01[:-1], label='alpha=0.01')
+	ax2.plot(a1[:-1], label='alpha=0.1')
+	ax2.legend(loc="upper left")
+
+	# plot inventory process with different phis
+	a0001,b0001 = run_almgren_chriss_with_constant_pi(phi=0.0001)
+	a001,b001 = run_almgren_chriss_with_constant_pi(phi=0.001)
+	a01,b01=run_almgren_chriss_with_constant_pi(phi=0.01)
+	a1,b1=run_almgren_chriss_with_constant_pi(phi=0.1)
+
+	f, ((ax1, ax2)) = plt.subplots(1, 2, sharex='col')
+	ax1.set(ylabel='Inventory', xlabel='time (minutes)')
+	ax1.plot(b0001, label='phi=0.0001')
+	ax1.plot(b001, label='phi=0.001')
+	ax1.plot(b01, label='phi=0.01')
+	ax1.plot(b1, label='phi=0.1')
+	ax1.legend(loc="upper right")
+
+	ax2.set(ylabel='Income earned', xlabel='time (minutes)')
+	ax2.plot(a0001[:-1], label='phi=0.0001')
+	ax2.plot(a001[:-1], label='phi=0.001')
+	ax2.plot(a01[:-1], label='phi=0.01')
+	ax2.plot(a1[:-1], label='phi=0.1')
+	ax2.legend(loc="upper left")
+
+	# plot 
+
+
+
+
