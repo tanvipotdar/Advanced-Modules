ad85e9ce3c6c8193a96bb3a49793476bbf79990b
Tanvi Potdar <tanvipotdar@users.noreply.github.com>
2019-05-15 16:20:11 +0100

    Update credit_grades.py

--- credit_grades.py
+++ credit_grades.py
@@ -4,21 +4,21 @@ from scipy.stats import norm
 
 
 # constants
-T = 5               # expiry
-N = 10000           # number of monte carlo simulations
+T = 100 # expiry
+N = 10000 # number of monte carlo simulations
 
-s0 = 100            # initial stock price value
-s_ref = 100         # reference stock price value
-s_ref_sigma = 0.35  # reference stock vol
-D = 10              # debt per share
-l = 0.5             # mean of global debt recovery rate/ default barrier
-l_sigma = 0.3       # vol of global debt recovery rate/ default barrier
-r = 0.03            # risk free rate of interest
-R = 0.5             # recovery rate on underlying credit
+s0 = 100 # initial stock price value
+s_ref = 100 # reference stock price value
+s_ref_sigma = 0.35 # reference stock vol
+D = 200 # debt per share
+l = 0.5 # mean of global debt recovery rate/ default barrier
+l_sigma = 0.3 # vol of global debt recovery rate/ default barrier
+r = 0.05 # risk free rate of interest
+R = 0.5 # recovery rate on underlying credit
 
 
 def g(x, d):
-    z = np.sqrt(0.25+(2*r)/s_ref_sigma**2)
+    z = np.sqrt(0.25 + 2 * r/s_ref_sigma**2)
     a = -np.log(d)/(s_ref_sigma*np.sqrt(x))
     b = z*s_ref_sigma*np.sqrt(x)
     param1 = d**(z+1/2)*norm.cdf(a-b)
@@ -26,21 +26,22 @@ def g(x, d):
     return param1 + param2
 
 
-def calculate_survival_probability():
+def calculate_survival_probability_and_spread():
     t = T*60
-    d = (s0 + l*D)/l*D) * np.exp(l_sigma**2)
+    d = (s0 + l*D)/(l*D) * np.exp(l_sigma**2)
     eta = l_sigma**2/s_ref_sigma**2
 
-    a = np.zeroes(t+1)
-    p = np.zeroes(t+1)
+    asset_price = np.zeros(t+1)
+    prob = np.zeros(t+1)
 
     for i in range(t+1):
-        param1 = s_ref_sigma * s_ref/(s_ref + l_sigma*D)
-        a[i] = np.sqrt(param1**2*(i/60) + l_sigma**2)
-        p[i] = norm.cdf(-a[i]/2 + np.log(d)/a[i]) - d*norm.cdf(-a[i]/2 - np.log(d)/a[i])
+        param1 = s_ref_sigma * s_ref/(s_ref + l*D)
+        asset_price[i] = np.sqrt(param1**2*(i/60) + l_sigma**2)
+        prob[i] = norm.cdf(-asset_price[i]/2 + np.log(d)/asset_price[i]) - d*norm.cdf(-asset_price[i]/2 - np.log(d)/asset_price[i])
 
-    numerator = 1 - p[0] + np.exp(r*eta)*(g(t+eta, d) - g(eta, d))
-    denominator = p[0] - p[T]*np.exp(-r*t) - np.exp(r*eta)*(g(t+eta, d) - g(eta, d))
+    import ipdb; ipdb.set_trace()
+    numerator = 1 - prob[0] + np.exp(r*eta)*(g(t+eta, d) - g(eta, d))
+    denominator = prob[0] - prob[t]*np.exp(-r*t) - np.exp(r*eta)*(g(t+eta, d) - g(eta, d))
     cds_spread = r*(1-R)*(numerator/denominator)
-    return p[T], cds_spread
+    return prob[t], cds_spread/0.0001
  
