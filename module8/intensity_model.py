7acf4f2aa7b1261802e79811b2d2c260fe88bb74
Tanvi Potdar <tanvipotdar@users.noreply.github.com>
2019-05-23 17:17:15 +0100

    Update intensity_model.py

--- module8/intensity_model.py
+++ module8/intensity_model.py
@@ -1,28 +1,46 @@
 import numpy as np
-from scipy import integrate
+from scipy import integrate, optimize
+
+r = 0.03
+t = 0
+mat = 1
+R = 0.0192
+L = 0.6
 
 
-def cds(t, mat, R, L, gamma, P):
+def bond_pricer(t, r, T):
+    return np.exp(-r*(T-t)/T)
+
+def cds(gamma):
 	'''
 	t - cds is priced as of this time
 	mat - maturity of the cds
 	R - cds spread
-	L - loss given default 
-	gamma - intensities for each maturity year 
+	L - loss given default
+	gamma - intensities for each maturity year per quarter
 	P - price of risk-free zero-coupon bond
 	'''
-	quarters_left = (mat-t)*4
-	Gamma = np.zeros(quarters_left)
-	for i in range(1, len(Gamma)):
-		Gamma[i] = Gamma[i-1] + gamma[i]
+	q = (mat-t)*4
+	gamma = [gamma]*q
+	Gamma = np.cumsum(gamma)
 
 	a1 = 0
 	a2 = 0
 	a3 = 0
 
-	for i in range(len(Gamma)):
-		a1 += gamma[i] + integrate.quad(np.exp(-Gamma[i] - gamma[i]*))
-		a2 += np.exp(-Gamma[i])
+	for i in range(1, q):
+		f1 = lambda u : np.exp(-Gamma[i-1] - gamma[i]*(u-(i-1)))*bond_pricer(u,r,q)*(u-(i-1))
+		a1 += gamma[i] + integrate.quad(f1, i-1, i)[0]
 
-		f3 = lambda i,u: np.exp(-Gamma[i-1] - gamma[i](u - (i-1))) * P(u*90)
-		a3 += gamma[i] * integrate.quad()
+		a2 += bond_pricer(i,r,q)*np.exp(-Gamma[i])
+
+		f3 = lambda u : np.exp(-Gamma[i-1] - gamma[i]*(u-(i-1)))*bond_pricer(u,r,q)
+		a3 += gamma[i] + integrate.quad(f1, i-1, i)[0]
+
+	a1 = a1*R
+	a2 = a2*R
+	a3 = L*a3
+
+	return a1 + a2 -a3
+
+x = optimize.fsolve(cds,0.0125)
