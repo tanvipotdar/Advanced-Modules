7a7c9524ff48c4a6107d71d8150850829bf4fec3
Tanvi Potdar <tanvipotdar@tanvis-imac.mynet>
2019-05-24 09:50:47 +0100

    completed creditgrades model

--- module8/creditgrades.py
+++ module8/creditgrades.py
@@ -1,12 +1,13 @@
 import numpy as np
 import pandas as pd
+from scipy.stats import norm
 
 
 def calculate_survival_probability(L_mean, S0, S_ref, sigma_ref, D, lmbda, t):
     d = (S0 + L_mean*D)/(L_mean*D) * np.exp(lmbda**2)
-    a_squared = (sigma*S_ref/(S_ref+L_mean*D))**2*t + lmbda**2
+    a_squared = (sigma_ref*S_ref/(S_ref+L_mean*D))**2*t + lmbda**2
     a_t = np.sqrt(a_squared)
-    prob = norm.cdf(-a[i]/2 + np.log(d)/a[i]) - d*norm.cdf(-a[i]/2 - np.log(d)/a[i])
+    prob = norm.cdf(-a_t/2 + np.log(d)/a_t) - d*norm.cdf(-a_t/2 - np.log(d)/a_t)
     return prob
 
 
@@ -21,16 +22,21 @@ def G(u, d, sigma_at, r):
     return param1 + param2
 
 
-def calculate_cds_spread(L_mean, S0, S_ref, sigma_ref, D, lmbda, r, R, t):
-    prob_0 = calculate_survival_probability(L_mean, S0, S-ref, sigma_ref, D, lmbda, 0)
-    prob_t = calculate_survival_probability(L_mean, S0, S-ref, sigma_ref, D, lmbda, t)
+def creditgrades(L_mean, S0, S_ref, sigma_ref, D, lmbda, r, R, t):
+    prob_0 = calculate_survival_probability(L_mean, S0, S_ref, sigma_ref, D, lmbda, 0)
+    prob_t = calculate_survival_probability(L_mean, S0, S_ref, sigma_ref, D, lmbda, t)
 
-    sigma_at = sigma*S_ref/(S_ref+L_mean*D)
-    eta = lmbda**2 / sigma**2
+    d = (S0 + L_mean*D)/(L_mean*D) * np.exp(lmbda**2)
+    sigma_at = sigma_ref*S_ref/(S_ref+L_mean*D) 
+    eta = lmbda**2 / sigma_at**2
 
-    x1 = np.exp(r*eta) * (G(t+eta, d, sigma_at, r) - G(eta, d. sigma_at, r))
+    x1 = np.exp(r*eta) * (G(t+eta, d, sigma_at, r) - G(eta, d, sigma_at, r))
     x2 = 1 - prob_0 + x1
     x3 = prob_0 - prob_t*np.exp(-r*t) - x1
 
+
     cds_spread = r*(1-R)*(x2/x3)*10000
-    return cds_spread
+    df = pd.DataFrame(data=[t, sigma_ref, S0, D, cds_spread, prob_t]).T
+    df.columns = ['maturity', 'equity_vol', 'S0', 'debt', 'cds_spread', 'survival_probability']
+    # return cds_spread, prob_t
+    return df
